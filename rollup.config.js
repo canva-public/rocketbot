23bfa12631de6ed5c4feed0afcb43fcbeda76d12
Joscha Feth <joscha@canva.com>
2021-05-06 00:25:16 +1000

    feat: use pino

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
-import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   plugins: [
@@ -7,6 +9,8 @@ export default {
       preferBuiltins: true,
     }),
     typescript(),
+    commonjs(),
+    json(),
   ],
   input: 'src/index.ts',
   output: {
