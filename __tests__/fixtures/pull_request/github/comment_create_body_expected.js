9e8a7e2f7747598b51c5cfef1188bf3b7f7d5931
Anh <anh@canva.com>
2021-10-15 14:33:01 +1100

    revert test fixtures

--- __tests__/fixtures/pull_request/github/comment_create_body_expected.js
+++ __tests__/fixtures/pull_request/github/comment_create_body_expected.js
@@ -6,44 +6,74 @@ module.exports = {
 
 By commenting on this PR with: \`:rocket:[<pipeline>]\`, e.g.
 
-
-
-<details>
-<summary>a-pipeline</summary>
-
+<table>
+<thead>
+<tr>
+<th>Comment</th>
+<th>Description</th>
+<th>More info</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>
 
 \`\`\`
 :rocket:[a-pipeline]
 \`\`\`
 
-[See more information](https://example.com/does-templating-work?COMMITISH=c0ffeec0ffeec0ffeec0ffeec0ffeec0ffeec0ffeec0ffe&ORG=some-org&REPO=some-repo)
+</td>
+<td>
 
-</details>
 
-<details>
-<summary>some-pipeline</summary>
 
+</td>
+<td>
+
+[:information_source:](https://example.com/does-templating-work?COMMITISH=c0ffeec0ffeec0ffeec0ffeec0ffeec0ffeec0ffeec0ffe&ORG=some-org&REPO=some-repo "See more information")
+
+</td>
+</tr>
+<tr>
+<td>
 
 \`\`\`
 :rocket:[some-pipeline]
 \`\`\`
 
-[Add more information](https://github.com/some-org/some-repo/new/master?filename=.buildkite%2Fpipeline%2Fdescription%2Fsome-org%2Fsome-pipeline.md&value=%23+some-pipeline%0A%0A%5BDocument+some-pipeline%27s+RocketBot+options+here%5D)
+</td>
+<td>
 
-</details>
 
-<details>
-<summary>some-pipeline-lite</summary>
 
-This is a proper description with a \\| pipe
+</td>
+<td>
+
+[:heavy_plus_sign:](https://github.com/some-org/some-repo/new/master?filename=.buildkite%2Fpipeline%2Fdescription%2Fsome-org%2Fsome-pipeline.md&value=%23+some-pipeline%0A%0A%5BDocument+some-pipeline%27s+RocketBot+options+here%5D "Add more information")
+
+</td>
+</tr>
+<tr>
+<td>
+
 \`\`\`
 :rocket:[some-pipeline-lite]
 \`\`\`
 
-[See more information](https://github.com/some-org/some-repo/blob/master/.buildkite/pipeline/description/some-org/some-pipeline-lite.md)
+</td>
+<td>
 
-</details>
-  
+This is a proper description with a \\| pipe
+
+</td>
+<td>
+
+[:information_source:](https://github.com/some-org/some-repo/blob/master/.buildkite/pipeline/description/some-org/some-pipeline-lite.md "See more information")
+
+</td>
+</tr>
+</tbody>
+</table>
 
 _Note: you can pass [custom environment variables](https://github.com/canva-public/rocketbot/blob/main/docs/guides/pass-in-variables.md) to some builds._
 
